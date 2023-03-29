0a2709c7d604b72efc5aaf09bd190a6b945c3de4
jerryOluranti <faluyijeremiah2018@gmail.com>
2023-03-29 10:48:51 +0100

    added crud endpoints

--- recipe-backend/app.js
+++ recipe-backend/app.js
@@ -2,9 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.post('/', (req, res) => {
-  res.send("Hello express 👋");
+app.use(express.json()); // Helps our app to accept json data
+
+const recipes = [];
+
+app.get('/get', (req, res) => {
+  res.send(recipes);
 });
+
+app.post('/add', (req, res) => {
+  recipes.push(req.body);
+  res.send("Recipe added!");
+})
+
+app.patch('/edit', (req, res) => {
+
+})
+
+app.delete('/delete', (req, res) => {
+
+})
 
 // http://localhost:4321/
 app.listen(3000, () => {
