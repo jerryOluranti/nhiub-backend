91e2e30f90610f67f3cae9ebd4f020450e020875
jerryOluranti <faluyijeremiah2018@gmail.com>
2023-05-09 11:24:23 +0100

    updating latest changes

--- recipe-backend/src/app.js
+++ recipe-backend/src/app.js
@@ -10,6 +10,14 @@ app.use(express.json()); // Helps our app to accept json data
 app.use('/users', userRouter);
 app.use('/recipes', recipesRouter);
 
+try {
+  // throw new Error("Just kidding 🥲");
+} catch (e) {
+  console.log(e.message);
+} finally {
+  console.log("e don happen 😩");
+}
+
 mongoose.connect(process.env.MONGO_URL).then(() => {
   app.listen(5000, () => {
     console.log("express server running...");
